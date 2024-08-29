eb9e0a43b5c445cdb798a8ed5b81adc12edf013d
DumbG1rlInLif3 <jennifer020605@edu.unipar.br>
2024-08-29 07:18:29 -0300

    Atividade2

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+            int idade = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println( "Digite sua idade: ");
+        idade = sc.nextInt();
+
+
+        if (idade>= 18) {
+            System.out.println("é maior de idade");
+
+        }else{
+            System.out.println("não é maior de idade");
+        }
+
+    }
+
+
+
+}
